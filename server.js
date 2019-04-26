d52f7d169c674a83e599288577bc5463ada65c28
BrooksPoltl <bpoltl1@gmail.com>
2019-04-26 16:05:37 -0500

    test

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use('/graphql',expressGraphQL({
     schema,
     graphiql: true
 }))
-app.use('/', (req,res)=> res.send('welcome to my project database'))
 const port = process.env.PORT || 4000;
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
