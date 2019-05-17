c42034f37cedbfe3566e78ee8159774aa91557c8
BrooksPoltl <bpoltl1@gmail.com>
2019-05-16 21:20:09 -0500

    finished contact page

--- client/src/Styles/Contact.js
+++ client/src/Styles/Contact.js
@@ -2,18 +2,105 @@
 import Styled from 'styled-components'
 
 export const Container = Styled.div`
-    height: 100vh;
+    height: ${props=>props.submitted? '100vh':'100%'};
+    min-height: 100vh;
     background: #222831;
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    z-index: -5;
 `
 
 export const Form = Styled.form`
-    height: 40vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-around;
     align-content: space-around;
-    width: 50%;
+    width: 40%;
+    margin-top: 30px;
+    margin-bottom: 40px;
+`
+export const Header = Styled.h1`
+    display:flex;
+    font-size: 50px;
+    color: white;
+    text-shadow: 5px 5px 5px black;
+    margin-top: 5%;
+    z-index: 0;
+`
+export const Field = Styled.p`
+    color: white;
+    margin-bottom: 5px;
+    text-shadow: 5px 5px 5px black;
+`
+export const ArgsInput = Styled.input`
+    height: 25px;
+    border-radius: 8px;
+    padding-left: 5px;
+    margin-bottom: 10px;
+    width: 100%;
+    &:focus{
+        outline: none;
+    }
+    border:${props => props.missing ? "2px solid red": "none"};
+`
+export const ArgsDiv = Styled.div`
+    width: 100%;
+`
+export const MessageInput =Styled.textarea`
+    border:${props => props.missing ? "2px solid red": "none"};
+    width: 100%;
+    border-radius: 8px;
+    padding-left: 5px;
+    padding-top: 5px;
+    resize:none;
+    font-family: sans-serif;
+    &:focus{
+        outline: none;
+    }
+`
+export const SubmitButton = Styled.button`
+    box-shadow: 5px 5px 5px black;
+    width: 150px;
+    height: 35px;
+    color: white;
+    background-color: #222831;
+    border: 2px solid white;
+    border-radius: 2px;
+    font-weight: bold;
+    font-size: 20px;
+    margin-top: 20px;
+    text-decoration: none;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    &:hover{
+        cursor: pointer;
+    }
+    &:active{
+        transform: translateY(4px); 
+        box-shadow: 2px 2px 2px black;
+    }
+    &:focus{
+        outline: none;
+    }
+`
+export const Icon = Styled.a`   
+    margin-top: 50px;
+    color: white;
+    font-size: 50px;
+    width: 20%;
+    margin-left: 46%;
+`
+export const SubmitText = Styled.p`
+    margin-top: 50px;
+    color: white;
+    width: 20%;
+    margin-left: 46%;
+`
+export const MissingField = Styled.p`
+    font-size: 20px;
+    color: red;
 `
--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import {Container, Form} from '../.././Styles/Contact'
+import {Container, Form, Header, Field, SubmitButton,MissingField, Icon, SubmitText, ArgsInput, ArgsDiv, MessageInput} from '../.././Styles/Contact'
 import gql from 'graphql-tag'
 import {Mutation} from 'react-apollo'
 import {Element} from 'react-scroll';
@@ -8,52 +8,88 @@ const Contact = () =>{
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
     const [company, setCompany] = useState("");
-
+    const [missing, setMissing] = useState("");
     return(
-        
             <Mutation mutation = {SEND_EMAIL}>
-            {(sendEmail, { data })=>(
-                <Element name = "Contact">
-                    <Container>
-                    <h2>Contact Me</h2>
+            {(sendEmail, {data, loading, error})=>(
+                    <Element name = "Contact">
+                        <Container submitted ={data ? true:false}>
+                        <Header>Contact Me</Header>
+                        { !data && !loading && 
                         <Form onSubmit = {async(event)=>{
                             event.preventDefault();
-                            const response = await sendEmail({ variables: {name: name, email: email, message: message, company: company}})
+                            console.log(event.target)
+                            if(!email.length || !message.length || !name.length ||!company.length){
+                                if(!name.length){
+                                    return setMissing("name")
+                                }
+                                else if(!email.length){
+                                    return setMissing("email")
+                                }
+                                else if(!company.length){
+                                    return setMissing("company")
+                                }else{
+                                    return setMissing("message")
+                                }
+                            }
+                            let response = null;
+                            response = await sendEmail({ variables: {name: name, email: email, message: message, company: company}})
                             setName("");
                             setEmail("");
                             setMessage("");
                             setCompany("");
-                            console.log(response)
+                            console.log(response);
                         }}>
-                            <input type = 'text' value = {name} placeholder = 'Name' onChange = {
-                                (event)=>{
-                                    event.preventDefault();
-                                    setName(event.target.value);
-                                }} 
-                                />
-                            <input type = 'text' value = {email} placeholder = 'Email' onChange = {
-                                (event)=>{
-                                    event.preventDefault();
-                                    setEmail(event.target.value);
+                            <ArgsDiv>
+                                <Field>Name:</Field>
+                                <ArgsInput type = 'text' missing = {missing ==='name'? true:false} value = {name} placeholder = 'Name' onChange = {
+                                    (event)=>{
+                                        event.preventDefault();
+                                        setName(event.target.value);
+                                    }} 
+                                    />
+                            </ArgsDiv>
+                            <ArgsDiv>
+                                <Field>Email:</Field>
+                                <ArgsInput type = 'text' missing = {missing === 'email'? true:false} value = {email} placeholder = 'Email' onChange = {
+                                    (event)=>{
+                                        event.preventDefault();
+                                        setEmail(event.target.value);
+                                    }
                                 }
-                            }
                             />
-                            <input type = 'text' value = {message} placeholder = 'Message' onChange = {
-                                (event)=>{
-                                    event.preventDefault();
-                                    setMessage(event.target.value);
-                                }
-                            }/>
-                            <input type = 'text' value = {company} placeholder = 'Company' onChange = {
+                            </ArgsDiv>
+                            <ArgsDiv>
+                            <Field>Company Name:</Field>
+                            <ArgsInput type = 'text' missing = {missing === 'company'?true:false} value = {company} placeholder = 'Company' onChange = {
                                 (event)=>{
                                     event.preventDefault();
                                     setCompany(event.target.value);
                                 }
                             }/>
-                            <button type = 'submit'>Send Message</button>
-                        </Form>
+                            </ArgsDiv>
+                            <ArgsDiv>
+                                <Field>Message:</Field>
+                                <MessageInput rows = "10" cols = "10" type = 'text' missing = {missing === 'message'?true:false} value = {message} placeholder = 'Message' onChange = {
+                                    (event)=>{
+                                        event.preventDefault();
+                                        setMessage(event.target.value);
+                                    }
+                                }/>
+                            </ArgsDiv>
+                            <SubmitButton type = 'submit'>Send Message</SubmitButton>
+                            {missing && <MissingField>missing: {missing}</MissingField>}
+                        </Form>}
+                    {loading && <div>
+                        <Icon>
+                            <i class="fas fa-spinner fa-spin"/>
+                        </Icon>
+                        <SubmitText>Loading...</SubmitText>
+                        </div>}
+                    {error && <SubmitText>error: could not submit form. Try refreshing and submitting again</SubmitText>}
+                    {data && <SubmitText>Your email has been submitted</SubmitText>}
                     </Container>
-                </Element>
+                </Element> 
                 )
             }
             </Mutation>
@@ -61,7 +97,7 @@ const Contact = () =>{
 }    
 const SEND_EMAIL = gql`
     mutation sendEmail($name: String!, $email: String!, $company: String!, $message: String!){
-        sendEmail(Name: $name, message: $message, email: $email, company: $company){
+        sendEmail(name: $name, message: $message, email: $email, company: $company){
             email
         }
     }
